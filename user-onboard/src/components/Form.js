3699fa71fdd4c139cda1487bc43f0305eacc78ec
Jonathan Scott <jonathan.scott@gmail.com>
2019-08-07 13:09:14 -0700

    steps 1-3 finished

---
+++ user-onboard/src/components/Form.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from "formik";
+import * as Yup from 'yup';
+
+
+function UserForm({errors, touched, values}) {
+    return(
+            <Form className='user-form'>
+                <Field type='text' name='name' placeholder='Please enter your name' />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+                <Field type='email' name='email' placeholder='Email' />
+                {touched.email && errors.email && (
+                    <p className="error">{errors.email}</p>
+                )}
+                <Field type='password' name='password' placeholder='Password' />
+                {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
+                <label className="checkbox-container">
+                Accept Terms of Service
+                <Field type="checkbox" name="tos" checked={values.tos} />
+                <span className="checkmark" />
+                </label>
+                {touched.tos && errors.tos && (
+                    <p className="error">{errors.tos}</p>
+                )}
+                <button type='submit'>Submit</button>
+            </Form>
+ 
+    )
+}
+const FormikUserForm = withFormik({
+    mapPropsToValues({name, email, password, tos}) {
+       return {
+        name: name || '',
+        email: email || '',
+        password: password || '',
+        tos: tos || false
+       }
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('You must enter a name').max(256, "Our database can't handle such an amazing name"),
+        email: Yup.string().required('You must enter an email').email('You must enter a vaild email'),
+        password: Yup.string().required('You must enter a password').min(5, 'You must enter a password at least 5 characters long'),
+        tos: Yup.boolean().oneOf([true], 'You must Accept Terms of Service')
+      }),
+
+    handleSubmit(values) {
+        console.log("form submitted", values);
+        axios.post(`https://reqres.in/api/users`, values)
+        .then(res => {
+            console.log('response', res)
+        })
+        .catch(error => {
+            console.log('Error', error)
+        })
+    }
+})(UserForm)
+
+export default FormikUserForm;
